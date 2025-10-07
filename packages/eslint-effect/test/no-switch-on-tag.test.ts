b9de99e944eb7864f45366ada16884435d13f999
Graeme Foster <80714+GraemeF@users.noreply.github.com>
2025-10-07 19:06:58 +0100

    fix(eslint-effect): add default cases to test switches for TypeScript exhaustiveness

--- packages/eslint-effect/test/no-switch-on-tag.test.ts
+++ packages/eslint-effect/test/no-switch-on-tag.test.ts
@@ -3,7 +3,7 @@ import { Either } from 'effect';
 const either = Either.right(42);
 
 type Event = { type: 'Created'; name: string } | { type: 'Updated'; name: string };
-const event: Event = { type: 'Created', name: 'test' };
+const event: Event = { type: 'Created', name: 'test' } as Event;
 
 // eslint-disable-next-line effect/no-switch-on-tag -- Testing switch on _tag
 switch (either._tag) {
@@ -19,20 +19,26 @@ switch (event.type) {
     break;
   case 'Updated':
     break;
+  default:
+    break;
 }
 
 // eslint-disable-next-line effect/no-switch-on-tag -- Testing switch on any value
-switch ('foo') {
+switch ('foo' as string) {
   case 'foo':
     break;
   case 'bar':
     break;
+  default:
+    break;
 }
 
 // eslint-disable-next-line effect/no-switch-on-tag -- Testing switch on number
-switch (42) {
+switch (42 as number) {
   case 42:
     break;
   case 0:
+    break;
+  default:
     break;
 }
